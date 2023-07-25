e2c8c39c5208ba49841385caf4c91f626bdc8b6c
ariel7198 <ariel.7198@hotmail.com>
2023-07-25 17:10:43 -0300

    adjusting vuetify components register on top level

--- src/main.js
+++ src/main.js
@@ -13,8 +13,20 @@ import { createApp } from 'vue'
 // Plugins
 import { registerPlugins } from '@/plugins'
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
+
 const app = createApp(App)
 
 registerPlugins(app)
+
+app.use(vuetify)
 
 app.mount('#app')
