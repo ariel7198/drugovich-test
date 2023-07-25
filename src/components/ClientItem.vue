325a61550940666bdef356bf6b473e75c7553bed
ariel7198 <ariel.7198@hotmail.com>
2023-07-24 21:32:19 -0300

    created clientItem component

---
+++ src/components/ClientItem.vue
@@ -0,0 +1,12 @@
+<template>
+
+<div> <p> {{ name }} | {{ cnpj }} | {{ status }}</p> </div>
+
+</template>
+
+<script> 
+    export default {
+        name: 'ClientItem',
+        props: ['name', 'cnpj','status']
+    }
+</script>
